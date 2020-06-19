50c12c3079ad8f055403ea1743101ac87bd7d04e
Silvio Manenti Junior <silviomjr@live.com>
2020-06-18 22:27:38 -0300

    Updated version!

--- somaDespesasReceitas.js
+++ somaDespesasReceitas.js
@@ -1,25 +1,25 @@
 const usuarios = [
     {
-      nome: "Salvio",
-      receitas: [115.3, 48.7, 98.3, 14.5],
-      despesas: [85.3, 13.5, 19.9]
+        nome: "Salvio",
+        receitas: [115.3, 48.7, 98.3, 14.5],
+        despesas: [85.3, 13.5, 19.9]
     },
     {
-      nome: "Marcio",
-      receitas: [24.6, 214.3, 45.3],
-      despesas: [185.3, 12.1, 120.0]
+        nome: "Marcio",
+        receitas: [24.6, 214.3, 45.3],
+        despesas: [185.3, 12.1, 120.0]
     },
     {
-      nome: "Lucia",
-      receitas: [9.8, 120.3, 340.2, 45.3],
-      despesas: [450.2, 29.9]
+        nome: "Lucia",
+        receitas: [9.8, 120.3, 340.2, 45.3],
+        despesas: [450.2, 29.9]
     }
 ];
 
 function somaNumeros(numeros) {
     let somaTotal = 0;
     for (var i in numeros) {
-        
+
         somaTotal = somaTotal + numeros[i]
     }
     return somaTotal
@@ -36,7 +36,7 @@ for (let i in usuarios) {
     saldo = saldo.toFixed(2)
     if (saldo > 0) {
         console.log(`${usuarios[i].nome} possui saldo POSITIVO de ${saldo}`);
-    } else if(saldo < 0) {
+    } else if (saldo < 0) {
         console.log(`${usuarios[i].nome} possui saldo NEGATIVO de ${saldo}`);
     } else {
         console.log(`${usuarios[i].nome} possui saldo ZERADO)`);
